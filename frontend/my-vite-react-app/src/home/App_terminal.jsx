54cacfc6a6045ba9737f7fc9d7f7b459072d4cc3
Unkno394 <akimenkovaleria403@gmail.com>
2025-04-12 19:24:42 +0300

    егор

--- frontend/my-vite-react-app/src/home/App_terminal.jsx
+++ frontend/my-vite-react-app/src/home/App_terminal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App_ssh.css';
 
 const ConnectionTabs = () => {
   const [activeTab, setActiveTab] = useState('ssh');
